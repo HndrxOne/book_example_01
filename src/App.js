e931d36cbb72f4689fa64e2044da4d4844eab60f
HndrxOne <jhlinaresv@outlook.com>
2022-11-11 07:33:22 -0600

    Book training

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const rootEl =
+  document.getElementById('root')
+
+
+let states = {}
+
+function _getM2(initialValue, key) {
+  if (states[key] === undefined) {
+    states[key] = initialValue
+  }
+  
+  return states[key]
 }
 
-export default App;
+function _setM2(v, key) {
+  states[key] = v
+  ReactDOM.render(<Title />, rootEl)
+}
+
+let prev
+
+function _onM(callback, value) {
+  if (value === prev) return 
+  callback()
+  prev = value
+}
+
+function Changed({ count }) {
+  let flag = 'N'
+  _onM(() => { flag = 'Y' }, count)
+
+  return <span>{flag}</span>
+}
+
+const Title = () => {
+  let countH = _getM2(0, 'H')
+  let countW = _getM2(0, 'W')
+
+  const onClickH = () => {
+    console.log('clickedH', countH)     
+    countH = countH + 1
+    _setM2(countH, 'H')
+  }
+
+  const onClickW = () => {
+    console.log('clickedW', countW)     
+    countW = countW + 1
+    _setM2(countW, 'W')
+  }
+
+  console.log('updatedH', countH)	      
+  console.log('updatedW', countW)	  
+  return (
+    <>
+      <button onClick={onClickH}>+</button>
+      <h1>Hello+{countH}</h1>
+      <Changed count={countH} />
+      <button onClick={onClickW}>+</button>
+      <h1>World+{countW}</h1>
+    </>
+  )
+}
+  
+function App() {
+  return <Title /> 
+}
+
+ReactDOM.render(<App />, rootEl)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootEl =
+  document.getElementById('root')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+let states = {}
+
+function _getM2(initialValue, key) {
+  if (states[key] === undefined) {
+    states[key] = initialValue
+  }
+  
+  return states[key]
+}
+
+function _setM2(v, key) {
+  states[key] = v
+  ReactDOM.render(<Title />, rootEl)
+}
+
+let prev
+
+function _onM(callback, value) {
+  if (value === prev) return 
+  callback()
+  prev = value
+}
+
+function Changed({ count }) {
+  let flag = 'N'
+  _onM(() => { flag = 'Y' }, count)
+
+  return <span>{flag}</span>
+}
+
+const Title = () => {
+  let countH = _getM2(0, 'H')
+  let countW = _getM2(0, 'W')
+
+  const onClickH = () => {
+    console.log('clickedH', countH)     
+    countH = countH + 1
+    _setM2(countH, 'H')
+  }
+
+  const onClickW = () => {
+    console.log('clickedW', countW)     
+    countW = countW + 1
+    _setM2(countW, 'W')
+  }
+
+  console.log('updatedH', countH)	      
+  console.log('updatedW', countW)	  
+  return (
+    <>
+      <button onClick={onClickH}>+</button>
+      <h1>Hello+{countH}</h1>
+      <Changed count={countH} />
+      <button onClick={onClickW}>+</button>
+      <h1>World+{countW}</h1>
+    </>
+  )
+}
+  
+function App() {
+  return <Title /> 
+}
+
+ReactDOM.render(<App />, rootEl)
